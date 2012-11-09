81deb62a440c63b05edb7683323db3f707360119
Patrick Lucas <me@patricklucas.com>
2012-11-09 18:18:43 -0500

    tabs to spaces

--- monitormanager/weakset.py
+++ monitormanager/weakset.py
@@ -2,18 +2,18 @@ import weakref
 
 class WeakSet(object):
 
-	def __init__(self):
-		super(WeakSet, self).__init__()
-		self._items = weakref.WeakKeyDictionary()
+    def __init__(self):
+        super(WeakSet, self).__init__()
+        self._items = weakref.WeakKeyDictionary()
 
-	def add(self, obj):
-		self._items[obj] = True
+    def add(self, obj):
+        self._items[obj] = True
 
-	def remove(self, obj):
-		del self._items[obj]
+    def remove(self, obj):
+        del self._items[obj]
 
-	def __iter__(self):
-		return iter(self._items)
+    def __iter__(self):
+        return iter(self._items)
 
-	def __len__(self):
-		return len(self._items)
+    def __len__(self):
+        return len(self._items)
