93fa578b5ef4e45d930cfaa38d30dcc4422d4e14
Patrick Lucas <me@patricklucas.com>
2012-11-12 00:03:14 -0500

    Add basic model

---
+++ monitormanager/model.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+ 
+Base = declarative_base()
+
+
+class Monitor(Base):
+
+    __tablename__ = 'monitors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+
+    def __init__(self, name, url="about:blank"):
+        self.name = name
+        self.url = url
+
+    def todict(self):
+        return {
+            'name': self.name,
+            'url': self.url,
+        }
+
+    def __repr__(self):
+        return "<Monitor(%s %s)>" % (self.name, self.url)
+
+
+engine = create_engine('sqlite:////tmp/monitormanager.db')
+Session = sessionmaker(bind=engine)
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
