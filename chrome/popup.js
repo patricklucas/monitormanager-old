79034d7ceb39a21816911bca149f400460bfc2c9
Patrick Lucas <me@patricklucas.com>
2012-11-15 16:41:04 -0500

    Basic backbone usage

--- chrome/popup.js
+++ chrome/popup.js
@@ -5,45 +5,54 @@
         return document.getElementById(id);
     };
 
-    var disableEvent = function(e) {
-        bg.mm_enable(!e.srcElement.checked);
-    };
-
-    var changeNameEvent = function() {
-        var newName = getEl('monitor_name').value;
-        bg.mm_setName(newName);
-    };
-
-    var changeServiceUrlEvent = function() {
-        var newServiceUrl = getEl('service_url').value;
-        bg.mm_setServiceUrl(newServiceUrl);
-    };
-
-    var openMonitorTabEvent = function(e) {
-        bg.mm_openTab();
-        e.srcElement.disabled = true;
-    };
-
     var initEvent = function(id, ev, callback) {
         getEl(id).addEventListener(ev, callback);
     };
 
-    var initEvents = function() {
-        initEvent('disable_check', 'change', disableEvent);
-        initEvent('monitor_name_save', 'click', changeNameEvent);
-        initEvent('service_url_save', 'click', changeServiceUrlEvent);
-        initEvent('open_monitor_tab', 'click', openMonitorTabEvent);
-    };
+    var TabControl = Backbone.Model.extend({
 
-    var initFormFields = function() {
-        getEl('disable_check').checked = !bg.mm_isEnabled();
-        getEl('monitor_name').value = bg.mm_getName();
-        getEl('service_url').value = bg.mm_getServiceUrl();
-        getEl('open_monitor_tab').disabled = bg.mm_isTabOpen();
-    };
+        initialize: function() {
+            this.initFormFields();
+            this.initEvents();
+        },
+
+        initFormFields: function() {
+            getEl('disable_check').checked = !bg.mm_isEnabled();
+            getEl('monitor_name').value = bg.mm_getName();
+            getEl('service_url').value = bg.mm_getServiceUrl();
+            getEl('open_monitor_tab').disabled = bg.mm_isTabOpen();
+        },
+
+        initEvents: function() {
+            initEvent('disable_check', 'change', this.disableEvent);
+            initEvent('monitor_name_save', 'click', this.changeNameEvent);
+            initEvent('service_url_save', 'click', this.changeServiceUrlEvent);
+            initEvent('open_monitor_tab', 'click', this.openMonitorTabEvent);
+        },
+
+        disableEvent: function(e) {
+            bg.mm_enable(!e.srcElement.checked);
+        },
+
+        changeNameEvent: function() {
+            var newName = getEl('monitor_name').value;
+            bg.mm_setName(newName);
+        },
+
+        changeServiceUrlEvent: function() {
+            var newServiceUrl = getEl('service_url').value;
+            bg.mm_setServiceUrl(newServiceUrl);
+        },
+
+        openMonitorTabEvent: function(e) {
+            bg.mm_openTab();
+            e.srcElement.disabled = true;
+        }
+
+    });
 
     document.addEventListener('DOMContentLoaded', function () {
-        initEvents();
-        initFormFields();
+        tabctl = new TabControl();
     });
+
 })();
