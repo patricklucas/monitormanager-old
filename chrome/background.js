ae9363f1f8f5698bcaadf79b7350ce004cbf4654
Patrick Lucas <me@patricklucas.com>
2012-11-16 16:55:17 -0500

    Improve correctness of config persistence system

--- chrome/background.js
+++ chrome/background.js
@@ -1,89 +1,63 @@
 (function(mm) {
 
-    var enabled = true;
-    var service_netloc = mm.defaults.service_netloc;
-    var monitorTabs = {};
+    mm.ready(function() {
+        var monitorTabs = {}; // tabId -> MonitorTab
 
-    // Try to load global parameters from storage
-    chrome.storage.local.get(['enabled', 'service_netloc'], function(items) {
-        var toSet = {};
-
-        if (items.enabled !== undefined) {
-            enabled = items.enabled;
-        } else {
-            toSet.enabled = enabled;
-        }
-
-        if (items.service_netloc !== undefined) {
-            service_netloc = items.service_netloc;
-        } else {
-            toSet.service_netloc = service_netloc;
-        }
-
-        // Set to defaults if unset
-        if (toSet) {
-            chrome.storage.local.set(toSet);
-        }
-    });
-
-    mm.getEnabled = function(enable) {
-        return enabled;
-    };
-
-    mm.setEnabled = function(enable) {
-        if (enable == enabled) {
-            return;
-        }
-
-        enabled = enable;
-
-        // Save enabled state to storage
-        chrome.storage.local.set({enabled: enabled});
-
-        for (tabId in monitorTabs) {
-            monitorTabs[tabId].setEnabled(enabled);
-        }
-    };
-
-    mm.getServiceNetloc = function() {
-        return service_netloc;
-    };
-
-    mm.setServiceNetloc = function(netloc) {
-        // Save new netloc to storage
-        chrome.storage.local.set({service_netloc: netloc});
-
-        for (tabId in monitorTabs) {
-            monitorTabs[tabId].setServiceNetloc(netloc);
+        mm.getEnabled = function() {
+            return mm.getConfig('enabled');
         };
-    };
 
-    mm.getMonitorTab = function(tabId) {
-        if (!tabId in monitorTabs) {
-            return null;
-        }
-
-        return monitorTabs[tabId];
-    };
-
-    mm.numOpenMonitorTabs = function() {
-        return Object.keys(monitorTabs).length;
-    };
-
-    // Time for some drinks
-    mm.openMonitorTab = function() {
-        var tab = new mm.MonitorTab({
-            enabled: enabled,
-            service_netloc: service_netloc,
-            monitor_name: mm.defaults.monitor_name,
-            monitor_url: mm.defaults.monitor_url,
-            onCreate: function() {
-                monitorTabs[tab.tab.id] = tab;
-            },
-            onRemove: function() {
-                delete monitorTabs[tab.tab.id];
+        mm.setEnabled = function(enable) {
+            if (enable == mm.getConfig('enabled')) {
+                return;
             }
-        });
-    };
+
+            mm.setConfig('enabled', enable);
+
+            for (tabId in monitorTabs) {
+                monitorTabs[tabId].setEnabled(enabled);
+            }
+        };
+
+        mm.getServiceNetloc = function() {
+            return mm.getConfig('service_netloc');
+        };
+
+        mm.setServiceNetloc = function(netloc) {
+            mm.setConfig('service_netloc', netloc);
+
+            for (tabId in monitorTabs) {
+                monitorTabs[tabId].setServiceNetloc(netloc);
+            };
+        };
+
+        mm.getMonitorTab = function(tabId) {
+            if (!tabId in monitorTabs) {
+                return null;
+            }
+
+            return monitorTabs[tabId];
+        };
+
+        mm.numOpenMonitorTabs = function() {
+            return Object.keys(monitorTabs).length;
+        };
+
+        // Time for some drinks (get it? tab? drinks?)
+        mm.openMonitorTab = function() {
+            var tab = new mm.MonitorTab({
+                enabled: mm.getConfig('enabled'),
+                service_netloc: mm.getConfig('service_netloc'),
+                monitor_name: mm.getConfig('default_monitor_name'),
+                monitor_url: mm.getConfig('default_monitor_url'),
+                onCreate: function() {
+                    monitorTabs[tab.tab.id] = tab;
+                },
+                onRemove: function() {
+                    delete monitorTabs[tab.tab.id];
+                }
+            });
+        };
+    });
 
 })(window.mm);
--- chrome/config.js
+++ chrome/config.js
@@ -1,11 +1,63 @@
-(function() {
+(function(root) {
 
-    window.mm = {
-        defaults: {
-            service_netloc: "localhost:8123",
-            monitor_name: "default",
-            monitor_url: "about:blank"
+    var mm = {};
+
+    var config = {
+        enabled: true,
+        service_netloc: "localhost:8123",
+        default_monitor_name: "default",
+        default_monitor_url: "about:blank"
+    };
+
+    var storageConfigKeys = ['enabled', 'service_netloc'];
+
+    var configLoaded = false;
+    var readyCallback = null;
+
+    // Simple "wait for config from storage" mechanism
+    mm.ready = function(callback) {
+        if (configLoaded) {
+            callback();
+        } else {
+            readyCallback = callback;
         }
     };
 
-})();
+    mm.getConfig = function(key) {
+        return config[key];
+    };
+
+    mm.setConfig = function(key, value) {
+        if (!key in storageConfigKeys) {
+            return;
+        }
+
+        config[key] = value;
+
+        var toSet = {};
+        toSet[key] = value;
+        chrome.storage.local.set(toSet);
+    };
+
+    var loadConfig = function() {
+        chrome.storage.local.get(storageConfigKeys, function(items) {
+            storageConfigKeys.forEach(function(key) {
+                if (items[key] !== undefined) {
+                    config[key] = items[key];
+                } else {
+                    mm.setConfig(key, config[key]);
+                }
+            });
+
+            configLoaded = true;
+            if (readyCallback) {
+                readyCallback();
+            }
+        });
+    };
+
+    loadConfig();
+
+    root.mm = mm;
+
+})(window);
