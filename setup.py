65650425f876dbe914d749c5ea8712d60ed6443a
Patrick Lucas <me@patricklucas.com>
2014-08-30 14:21:02 -0700

    Improve setup.py, requirements.txt

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='monitormanager',
-    version='0.1',
-    provides=['monitormanager'],
+    name="monitormanager",
+    version="0.1",
     author="Patrick Lucas",
     author_email="plucas@yelp.com",
     description="Monitor management service",
-    packages=['monitormanager'],
     long_description="Controls the URL of a tab running in a browser "
         "connected via websockets.",
+    packages=["monitormanager"],
+    install_requires=[
+        "PyStaticConfiguration",
+        "PyYAML",
+        "SQLAlchemy",
+        "simplejson",
+        "tornado",
+    ]
 )
